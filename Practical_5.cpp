d0a654b6c0525fa44665cce926d1c7fa7e845a3a
Hrishit Dey <hrishitdey@gmail.com>
2025-11-29 12:06:43 +0530

    queue adt

--- Practical_5.cpp
+++ Practical_5.cpp
@@ -21,19 +21,19 @@ public:
         count = 0;
     }
     void enqueue(int value) {
-        Node* newNode = new Node(value);
+        Node* n = new Node(value);
         if (rear == nullptr) { // empty queue
-            front = rear = newNode;
+            front = rear = n;
         } else {
-            rear->next = newNode;
-            rear = newNode;
+            rear->next = n;
+            rear = n;
         }
         count++;
     }
-    int dequeue() {
+    void dequeue() {
         if (front == nullptr) {
-            cerr << "Queue Underflow\n";
-            exit(1);
+            cout << "Queue Underflow\n";
+            return;
         }
         int value = front->data;
         Node* temp = front;
@@ -43,12 +43,11 @@ public:
         }
         delete temp;
         count--;
-        return value;
     }
     int Front() { // function to access front element
         if (front == nullptr) {
-            cerr << "Queue is empty\n";
-            exit(1);
+            cout << "Queue is empty\n";
+            return -1;
         }
         return front->data;
     }
@@ -58,14 +57,28 @@ public:
     int size() {
         return count;
     }
+    void Display() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
 };
 int main() {
     Queue q;
     q.enqueue(10);
     q.enqueue(20);
     q.enqueue(30);
+    q.Display();
     cout << "Front: " << q.Front() << endl; 
     q.dequeue();
+    q.Display();
     cout << "Front after dequeue: " << q.Front() << endl; 
     return 0;
 }
